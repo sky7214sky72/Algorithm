f403153253075516e2fe60d9f6c811366b64b398
WonSeok <sky7214sky72@naver.com>
2021-09-16 11:27:44 +0900

    신규아이디 추천(내방식으로 추천)

--- 구현/신규아이디추천.py
+++ 구현/신규아이디추천.py
@@ -1,34 +1,32 @@
 def solution(new_id):
     answer = ''
-
-    # STEP 1
-    new_id = new_id.lower()
-
-    # STEP 2
-    for ch in new_id:
-        if ch.isalpha() or ch.isdigit() or ch in ['-', '_', '.']:
-            answer += ch
-
-    # STEP 3
+    #1step
+    new_id  = new_id.lower()
+    #2step
+    for i in new_id:
+        if i.isalpha() or i.isdigit() or i in ['-','_','.']:
+            answer += i
+    #3step
     while '..' in answer:
-        answer = answer.replace('..', '.')
-
-    # STEP 4
+        answer = answer.replace('..','.')
+    #4step
     if answer[0] == '.':
-        answer = answer[1:] if len(answer) > 1 else '.'
-    if answer[-1] == '.':
+        answer = answer[1:]
+        #처음과 끝이 모두 .인 경우
+        if len(answer) != 0 and answer[-1] == '.':
+            answer = answer[:-1]
+    elif answer[-1] == '.':
         answer = answer[:-1]
-
-    # STEP 5 ~ 6
-    answer = 'a' if len(answer) == 0 else answer[:15]
-
-    # STEP 6
-    if answer[-1] == '.':
-        answer = answer[:-1]
-
-    # STEP 7
-    while len(answer) < 3:
-        answer += answer[-1]
-
+    #5step
+    if len(answer) == 0:
+        answer = 'a'
+    #6step
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    #7step
+    if len(answer) <= 2:
+        while len(answer) < 3:
+            answer = answer + answer[-1]
     return answer
-
