630ddc3bf40b2b77e3550a22f24f7975a11bc122
choiwonseok <sky7214sky72@naver.com>
2022-04-19 23:25:51 +0900

    20220419 백준(쉬운거)

--- acmicpc/2438.py
+++ acmicpc/2438.py
@@ -7,8 +7,8 @@
 """
 
 n = int(input())
-for i in range(n):
-    for j in range(i+1):
+for i in range(1, n+1):
+    for j in range(i):
         print('*', end='')
-    print(' ')
+    print('')
 
--- acmicpc/2439.py
+++ acmicpc/2439.py
@@ -6,6 +6,11 @@
 *****
 """
 n = int(input())
-for i in range(1,n+1):
-    print(' '*(n-i),end=' ')
-    print('*'* i)
+for i in range(n, 0, -1):
+    for j in range(n):
+        if j <= i-2:
+            print('', end=' ')
+        if j > i-2:
+            print('*', end='')
+    print('')
+
