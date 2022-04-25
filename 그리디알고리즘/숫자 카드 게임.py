61a56fa379c44eb6103fce155b43f3cd32153b30
choiwonseok <sky7214sky72@naver.com>
2022-04-26 00:19:52 +0900

    20220426 이것이코딩테스트다책 그리디 알고리즘

--- 그리디알고리즘/숫자 카드 게임.py
+++ 그리디알고리즘/숫자 카드 게임.py
@@ -1,11 +1,10 @@
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 result = 0
-
 for i in range(n):
-    a = list(map(int,input().split()))
-    min_value = min(a)
-    if result < min_value:
-        result = min_value
+    a = list(map(int, input().split()))
+    minNum = min(a)
+    if result < minNum:
+        result = minNum
 
 print(result)
 
