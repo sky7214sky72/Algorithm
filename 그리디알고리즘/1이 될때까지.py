c627238d057406cdcf4654d8b56b515b34d2facf
choiwonseok <sky7214sky72@naver.com>
2022-04-27 23:34:14 +0900

    20220427 이것이코딩테스트다책 그리디 알고리즘

--- 그리디알고리즘/1이 될때까지.py
+++ 그리디알고리즘/1이 될때까지.py
@@ -1,18 +1,12 @@
-n,k = map(int,input().split())
+n, k = map(int, input().split())
 result = 0
 
-while True:
-    #N을 K로 나누어 떨어지는 수가 될때까지 빼기
-    target = (n//k)*k
-    result += (n-target)
-    n = target
-    #N이 K보다 낮으면 종료
-    if n<k:
-        break
-    #K로 나누기
-    result+=1
-    n //= k
+while n >= k:
+    if n % k != 0:
+        n -= 1
+        result += 1
+    else:
+        n = n // k
+        result += 1
 
-#마지막으로 남은 수에 대해 1씩 빼기
-result += (n-1)
 print(result)
