c39655cd5d68775605e44a29ea31eb7bf5871e61
choiwonseok <sky7214sky72@naver.com>
2022-05-18 00:46:52 +0900

    20220518 이것이코딩테스트다책 그리디 알고리즘

--- 구현/상하좌우.py
+++ 구현/상하좌우.py
@@ -1,10 +1,10 @@
 n = int(input())
-x,y=1,1
-plans = input().split()
+x, y = 1, 1
+plans = list(input().split())
 
-dx=[0,0,-1,1]
-dy=[-1,1,0,0]
-moves_types = ['L','R','U','D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+moves_types = ['L', 'R', 'U', 'D']
 
 for plan in plans:
     for i in range(len(moves_types)):
@@ -13,7 +13,6 @@ for plan in plans:
             ny = y + dy[i]
     if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
-    x,y = nx,ny
+    x, y = nx, ny
 
-print(x,y)
-
+print(x, y)
