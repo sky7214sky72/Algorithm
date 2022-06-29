41ae0f223de737f7b31952301111e7ab06169a2c
choiwonseok <sky7214sky72@naver.com>
2022-06-30 00:58:45 +0900

    20220630 이것이코딩테스트다책 dfs , bfs

--- DFS와BFS/특정거리의도시찾기.py
+++ DFS와BFS/특정거리의도시찾기.py
@@ -1,7 +1,6 @@
 from collections import deque
 n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n+1)]
-visited = [[] for _ in range(n+1)]
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
