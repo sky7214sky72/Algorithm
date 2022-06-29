1e8467d4cc70f188b05d003c53acd3a2b5c59c58
choiwonseok <sky7214sky72@naver.com>
2022-06-30 00:17:35 +0900

    20220630 이것이코딩테스트다책 dfs , bfs

--- DFS와BFS/BFS 예제.py
+++ DFS와BFS/BFS 예제.py
@@ -2,16 +2,11 @@ from collections import deque
 
 #BFS 메서드 정의
 def bfs(graph,start,visited):
-    #큐 구현을 위해 deque 라이브러리 사용
     queue = deque([start])
-    #현재 노드를 방문 처리
     visited[start] = True
-    #큐가 빌 때까지 반복
     while queue:
-        #큐에서 하나의 원소를 뽑아 출력하기
         v = queue.popleft()
-        print(v,end=' ')
-        #아직 방문하지 않은 인접한 원소들을 큐에 삽입
+        print(v, end=' ')
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
--- DFS와BFS/미로탈출.py
+++ DFS와BFS/미로탈출.py
@@ -1,20 +1,21 @@
 from collections import deque
 
-n,m = map(int, input().split())
+n, m = map(int, input().split())
 
-#2차원 리스트의 맵 정보 입력받기
+# 2차원 리스트의 맵 정보 입력받기
 graph = []
 for i in range(n):
-    graph.append(list(map(int,input())))
+    graph.append(list(map(int, input())))
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def bfs(x,y):
+
+def bfs(x, y):
     queue = deque()
-    queue.append((x,y))
+    queue.append((x, y))
     while queue:
-        x,y = queue.popleft()
+        x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -24,7 +25,8 @@ def bfs(x,y):
                 continue
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx,ny))
-    return graph[n - 1][m - 1]
+                queue.append((nx, ny))
+    return graph[n-1][m-1]
 
-print(bfs(0,0))
+
+print(bfs(0, 0))
