6dcaa48ad68abb9839f62f7631051d8f6185ac64
choiwonseok <sky7214sky72@naver.com>
2022-07-01 17:24:22 +0900

    20220630 이것이코딩테스트다책 dfs , bfs

--- DFS와BFS/특정거리의도시찾기.py
+++ DFS와BFS/특정거리의도시찾기.py
@@ -1,19 +1,23 @@
 from collections import deque
+
 n, m, k, x = map(int, input().split())
 graph = [[] for _ in range(n+1)]
+
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
-    distance = [-1] * (n+1)
-    distance[x] = 0
+
+distance = [-1] * (n+1)
+distance[x] = 0
 
 queue = deque([x])
 while queue:
     now = queue.popleft()
-    for i in graph[now]:
-        if distance[i] == -1:
-            distance[i] = distance[now] + 1
-            queue.append(i)
+    for next_node in graph[now]:
+        if distance[next_node] == -1:
+            distance[next_node] = distance[now] + 1
+            queue.append(next_node)
+
 check = False
 for i in range(1, n+1):
     if distance[i] == k:
