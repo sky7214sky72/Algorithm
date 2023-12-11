514f38034c82971a3d5b6b1974c7a280b5e7174e
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2023-12-11 23:01:08 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("|\\_/|");
+        bw.newLine();
+        bw.write("|q p|   /}");
+        bw.newLine();
+        bw.write("( 0 )\"\"\"\\");
+        bw.newLine();
+        bw.write("|\"^\"`    |");
+        bw.newLine();
+        bw.write("||_/=\\\\__|");
+        bw.flush();
+        bw.close();
+    }
+}
