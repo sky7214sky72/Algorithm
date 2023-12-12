275af84d3f6cdcf4593b35315aa0dda6523444ce
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2023-12-12 13:32:41 +0900

    [Bronze V] Title: N 찍기, Time: 1004 ms, Memory: 29520 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        for (int i = 1; i<=a;i++) {
+            System.out.println(i);
+        }
+    }
+}
