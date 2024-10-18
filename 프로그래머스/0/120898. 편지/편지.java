fa85bb8d155cc7d45be4bbadc55e0b79241d77f3
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2024-10-18 15:07:32 +0900

    [level 0] Title: 편지, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int solution(String message) {
+        char[] m = message.toCharArray();
+        int answer = m.length * 2;
+        return answer;
+    }
+}
