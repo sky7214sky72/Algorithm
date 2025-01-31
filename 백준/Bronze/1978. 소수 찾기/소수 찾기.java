28c683d60f3c525efe5dca22b6d02308818b197f
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2025-01-31 13:40:21 +0900

    [Bronze II] Title: 소수 찾기, Time: 168 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -2,27 +2,50 @@ import java.util.*;
 
 class Main {
 
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    int numInt = Integer.parseInt(scan.nextLine());
-    String numString = scan.nextLine();
-    String[] numArray = numString.split(" ");
-    int result = 0;
-    for (String num : numArray) {
-      int n = Integer.parseInt(num);
-      int count = 0;
-      if (n <= 1) {
-        continue;
-      }
-      for (int i = 2; i < n; i++) {
-        if (n % i == 0) {
-          count++;
-        }
-      }
-      if (count == 0) {
-        result++;
-      }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numInt = Integer.parseInt(scan.nextLine());
+        String numString = scan.nextLine();
+        String[] numArray = numString.split(" ");
+        int maxNum = findMax(numArray); // 입력된 숫자 중 최댓값을 찾음
+        boolean[] isPrime = sieveOfEratosthenes(maxNum); // 에라토스테네스의 체로 소수 배열 생성
+        int primeCount = countPrimes(numArray, isPrime);
+        System.out.println(primeCount);
     }
-    System.out.println(result);
-  }
+
+    private static int findMax(String[] numArray) {
+        int max = 0;
+        for (String num : numArray) {
+            int n = Integer.parseInt(num);
+            if (n > max) {
+                max = n;
+            }
+        }
+        return max;
+    }
+
+    private static boolean[] sieveOfEratosthenes(int maxNum) {
+        boolean[] isPrime = new boolean[maxNum + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false; // 0과 1은 소수가 아님
+        for (int i = 2; i * i <= maxNum; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= maxNum; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        return isPrime;
+    }
+
+    private static int countPrimes(String[] numArray, boolean[] isPrime) {
+        int primeCount = 0;
+        for (String num : numArray) {
+            int n = Integer.parseInt(num);
+            if (isPrime[n]) {
+                primeCount++;
+            }
+        }
+        return primeCount;
+    }
 }
