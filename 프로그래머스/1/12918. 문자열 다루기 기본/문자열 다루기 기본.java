ac139f3ad019e4219f60e33ffe0783224ef11a2d
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2025-04-04 14:14:34 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String s) {
+        if (s.length() != 4 && s.length() != 6) {
+	        return false;
+        }
+        
+        for (int i=0;i<s.length();i++) {
+	        if (!Character.isDigit(s.charAt(i))) {
+		        return false;
+	        }
+        }
+        
+        return true;
+    }
+}
