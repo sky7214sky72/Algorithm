7f5bce52c9e328be65257ac09a795777866b263b
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2025-05-22 19:18:58 +0900

    [level 1] Title: 모의고사, Time: 4.32 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 83.6 MB, 시간: 6.33 ms
+메모리: 74.8 MB, 시간: 4.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 28일 09:47:03
+2025년 05월 22일 19:18:56
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,24 +1,27 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-        int[] first = {1,2,3,4,5};
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] students = new int[3];
+        int[][] pattern = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
+        int[] scores = new int[3];
         for (int i=0;i<answers.length;i++) {
-            if (answers[i] == first[i % first.length]) students[0]++;
-            if (answers[i] == second[i % second.length]) students[1]++;
-            if (answers[i] == third[i % third.length]) students[2]++;
-        }
-        int maxScore = Math.max(students[0], Math.max(students[1], students[2]));
-        List<Integer> answer = new ArrayList<>();
-        int index = 1;
-        for (int student : students) {
-            if (maxScore == student) {
-                answer.add(index);
+            for (int j=0;j<pattern.length;j++) {
+                if (answers[i] == pattern[j][i % pattern[j].length]) {
+                    scores[j]++;
+                }
             }
-            index++;
         }
-        return answer.stream().mapToInt(i -> i).toArray();
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        List<Integer> answer = new ArrayList<>();
+        for (int i=0;i<scores.length;i++) {
+            if (maxScore == scores[i]) {
+                answer.add(i+1);
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
