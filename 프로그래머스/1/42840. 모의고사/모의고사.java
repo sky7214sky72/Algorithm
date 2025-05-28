0fbf569ec81d0508b71305a9b0ed4f268d9f656a
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2025-05-28 12:28:55 +0900

    [level 1] Title: 모의고사, Time: 8.98 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,27 +1,28 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] pattern = {
+        //수포자 찍기 패턴 정리
+        int[][] people = {
             {1,2,3,4,5},
             {2,1,2,3,2,4,2,5},
             {3,3,1,1,2,2,4,4,5,5}
         };
         int[] scores = new int[3];
-        for (int i=0;i<answers.length;i++) {
-            for (int j=0;j<pattern.length;j++) {
-                if (answers[i] == pattern[j][i % pattern[j].length]) {
-                    scores[j]++;
+        int maxScore = 0;
+        for (int i=0;i<people.length;i++) {
+            for (int j=0;j<answers.length;j++) {
+                if (answers[j] == people[i][j % people[i].length]) {
+                    scores[i] +=1;
                 }
             }
+            maxScore = Math.max(maxScore, scores[i]);
         }
-        int maxScore = Arrays.stream(scores).max().getAsInt();
         List<Integer> answer = new ArrayList<>();
         for (int i=0;i<scores.length;i++) {
-            if (maxScore == scores[i]) {
+            if (scores[i] == maxScore) {
                 answer.add(i+1);
             }
         }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
