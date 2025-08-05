5c1b53579e59394ab1bbfea08d84b159899c3cfb
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2025-08-05 13:17:58 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 20108 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,15 +1,9 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws Exception {
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String a = br.readLine();
-        StringTokenizer st = new StringTokenizer(a);
-        int result = 0;
-        while (st.hasMoreTokens()) {
-            st.nextToken();
-            result++;
-        }
-        System.out.println(result);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(st.countTokens());
     }
 }
