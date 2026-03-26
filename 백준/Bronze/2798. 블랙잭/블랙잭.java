c3181b3518bffa8eda765113c99ac7de68109118
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2026-03-26 13:20:10 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,34 +1,36 @@
 import java.util.*;
 import java.io.*;
 class Main {
+    static int N,M;
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        
         st = new StringTokenizer(br.readLine());
-        for (int num = 0;num<N;num++) {
-            arr[num] = Integer.parseInt(st.nextToken());
+        for (int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int answer = 0;        
+        int result = 0;
+        // 0,1,2
         for (int i=0;i<N-2;i++) {
+            //1,2,3
             for (int j=i+1;j<N-1;j++) {
+                //2,3,4
                 for (int k=j+1;k<N;k++) {
                     int sum = arr[i] + arr[j] + arr[k];
                     if (sum == M) {
                         System.out.println(sum);
                         return;
                     }
-                    if (sum < M && sum > answer) {
-                        answer = sum;
+                    if (sum < M && sum > result) {
+                        result = sum;
                     }
                 }
             }
         }
-        System.out.println(answer);
+        System.out.println(result);
     }
 }
