98f135a16a44a88349eae30441d527b364532aa9
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2026-04-10 09:52:23 +0900

    [level 1] Title: 폰켓몬, Time: 6.13 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,11 +1,13 @@
 import java.util.*;
-class Solution {
+
+class Solution {    
     public int solution(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.put(n, n);
+        int answer = 0;
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int num : nums) {
+            numMap.put(num, num);
         }
-        int answer = nums.length / 2 >= map.size() ? map.size() : nums.length / 2;
+        answer = nums.length / 2 >= numMap.size() ? numMap.size() : nums.length / 2;
         return answer;
     }
 }
