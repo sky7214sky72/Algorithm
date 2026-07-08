9500ebca865bbe690c7e9220d9c7bf58af8cfd9b
최원석 <45224987+sky7214sky72@users.noreply.github.com>
2026-07-08 17:41:21 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
-        //통분
-        int commonNumer = numer1 * denom2 + numer2 * denom1;
+        int commonNumer = (numer1 * denom2) + (numer2 * denom1);
         int commonDenom = denom1 * denom2;
-        int gcd = getGcd(commonNumer, commonDenom);
+        int gcd = gcd(commonNumer, commonDenom);
         answer[0] = commonNumer / gcd;
         answer[1] = commonDenom / gcd;
         return answer;
     }
     
-    public int getGcd(int a, int b) {
+    public int gcd(int a, int b) {
         while (b != 0) {
             int r = a % b;
             a = b;
